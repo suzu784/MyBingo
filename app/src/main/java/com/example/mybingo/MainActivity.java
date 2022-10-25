a8c0e6c6f90519eda01e0b8ee74e2bf37ebb2996
suzu784 <s.suzuki784@hotmail.com>
2022-10-25 16:16:35 +0900

    ランダムな数字を表示

--- app/src/main/java/com/example/mybingo/MainActivity.java
+++ app/src/main/java/com/example/mybingo/MainActivity.java
@@ -7,6 +7,7 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,6 +20,12 @@ public class MainActivity extends AppCompatActivity {
     // 最大値の設定ボタン
     private Button registerMaxNumberButton;
 
+    // 次の数字を出すボタン
+    private Button nextNumberButton;
+
+    // 現在の数字を表示するTextView
+    private TextView currentNumberTextView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,8 +33,9 @@ public class MainActivity extends AppCompatActivity {
 
         // ビューの変数を初期化する
         maxNumberEditText = findViewById(R.id.max_number);
-
         registerMaxNumberButton = findViewById(R.id.register_max_number);
+        nextNumberButton = findViewById(R.id.next_number);
+        currentNumberTextView = findViewById(R.id.current_number);
 
         // 最大値の初期値をEditTextにセットする
         maxNumberEditText.setText("" + maxNumber);
@@ -45,6 +53,25 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        // 表示中の数字を更新する
+        nextNumberButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onClickNextNumber();
+            }
+        });
+    }
+    // next_numberのボタンがタップされた時の処理
+    private void onClickNextNumber() {
+        Log.d("MainActivity", "onClickNextNumber");
 
+        // 0.0~74.0(最大値が初期値の場合の)数値を生成する
+        double randomNumber = Math.random() * (maxNumber - 1);
+
+        // 1~75(最大値が初期値の場合)の整数値を作成する
+        int nextNumber = (int)randomNumber + 1;
+
+        // nextNumberを画面に表示する
+        currentNumberTextView.setText("" + nextNumber);
     }
 }
